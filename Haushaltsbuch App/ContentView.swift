10a3de3b88e303d6a2c385ab54867a92d5f463d4
Burak <b.cuece@web.de>
2022-03-25 08:45:33 +0100

    Initial Commit

---
+++ Haushaltsbuch App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Haushaltsbuch App
+//
+//  Created by Burak Cüce on 25.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Haushaltsbuch App/Haushaltsbuch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haushaltsbuch_AppApp.swift
+//  Haushaltsbuch App
+//
+//  Created by Burak Cüce on 25.03.22.
+//
+
+import SwiftUI
+
+@main
+struct Haushaltsbuch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
