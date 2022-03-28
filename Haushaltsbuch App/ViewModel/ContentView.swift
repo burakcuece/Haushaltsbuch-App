480ce1e54facc69b2daa62e93b99aa9b61c33449
Burak <b.cuece@web.de>
2022-03-28 13:19:56 +0200

    - delete HomeView - edit CustomTabView

--- Haushaltsbuch App/ViewModel/ContentView.swift
+++ Haushaltsbuch App/ViewModel/ContentView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         
-        HomeView()
+        VStack {
+            
+            ZStack {
+                
+                CustomTabView()
+            }
+        }
         
     }
 }
--- Haushaltsbuch App/ViewModel/CustomTabView.swift
+++ Haushaltsbuch App/ViewModel/CustomTabView.swift
@@ -8,18 +8,63 @@
 import SwiftUI
 
 struct CustomTabView: View {
+    
     var body: some View {
-        
-        ZStack {
-            HStack {
-                Image(systemName: "house.fill")
-                    .resizable()
-                    .frame(width: 30, height: 30)
+        NavigationView {
+            TabView() {
+                ZStack {
+                    
+                    Color("background")
+                    
+                    VStack (spacing: 20) {
+                        
+                        Image(systemName: "tray.fill")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 50, height: 50)
+                        
+                        Text("Keine Einträge")
+                            .font(.largeTitle)
+                        
+                    }
+                    
+                }
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+                
+                Text("Diagramm")
+                    .tabItem {
+                        Image(systemName: "circle")
+                        Text("Diagramm")
+                    }
+                Text("Eintrag hinzufügen")
+                    .tabItem {
+                        Image(systemName: "plus")
+                    }
+                Text("Kategorien")
+                    .tabItem {
+                        Image(systemName: "square.grid.2x2")
+                        Text("Kategorien")
+                    }
+                Text("Konto hinzufügen")
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("Profil")
+                    }
+                
+                
+                
             }
+            .navigationTitle("Haushaltsbuch App")
+            .navigationBarTitleDisplayMode(.inline)
+
         }
-        
     }
+    
 }
+
 
 struct CustomTabView_Previews: PreviewProvider {
     static var previews: some View {
