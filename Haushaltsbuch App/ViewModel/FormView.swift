b285dfa7acd68b856287a6d2a849ca9511b33ce3
Burak <b.cuece@web.de>
2022-04-13 10:33:01 +0200

    - edit Form View

--- Haushaltsbuch App/ViewModel/FormView.swift
+++ Haushaltsbuch App/ViewModel/FormView.swift
@@ -12,45 +12,55 @@ struct FormView: View {
     @State private var titel = ""
     @State private var betrag = NumbersOnly()
     @State private var kategorie = ""
-    @State private var einausgabe = ""
+    @State private var selectedInOutPut = "Eingabe"
     @State private var notiz = ""
     
+    var inoutput = ["Eingabe", "Ausgabe"]
+    
+    
     var body: some View {
+        
+        Form {
             
-            Form {
+            Section {
                 
-                Section {
-                    
-                    TextField("Titel", text: $titel)
-                    
-                }
+                TextField("Titel", text: $titel)
                 
-                Section {
-                    
-                    TextField("Betrag", text: $betrag.value)
-                        .keyboardType(.numberPad)
-                    
-                }
+            }
+            
+            Section {
                 
-                Section {
-                    TextField("Kategorie", text: $kategorie)
-                }
+                TextField("Betrag", text: $betrag.value)
+                    .keyboardType(.numberPad)
                 
-                Section {
-                    TextField("Eingabe / Ausgabe", text: $einausgabe)
-                }
+            }
+            
+            Section {
+                TextField("Kategorie", text: $kategorie)
+            }
+            
+            Section {
+                Picker(
+                    selection: $selectedInOutPut,
+                    label: Text("Bitte auswählen"),
+                    content: {
+                        Text("Eingabe").tag("1")
+                        Text("Ausgabe").tag("2")
+                })
+            }
+        
+            
+            Section {
+                TextField("Notiz", text: $notiz)
+                    .frame(height: 150, alignment: .topTrailing)
+            }
+            
+            Button(action: {
                 
-                Section {
-                    TextField("Notiz", text: $notiz)
-                        .frame(height: 150, alignment: .topTrailing)
-                }
+            }) {
                 
-                Button(action: {
-                    
-                }) {
-                    
-                    Text("Hinzufügen")
-                    
+                Text("Hinzufügen")
+                
                 
             }
             
@@ -62,3 +72,4 @@ struct FormView_Previews: PreviewProvider {
         FormView()
     }
 }
+
